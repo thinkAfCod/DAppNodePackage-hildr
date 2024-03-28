0d94489839b4c8e7e25155b63569e7d273fae464
thinkAfCod <q315xia@163.com>
2024-03-28 21:21:11 +0800

    support optimism-java/hildr modifications

--- op-node/entrypoint.sh
+++ op-node/entrypoint.sh
@@ -7,15 +7,19 @@ elif [ ! -z "$_DAPPNODE_GLOBAL_EXECUTION_CLIENT_MAINNET" ]; then
   case $_DAPPNODE_GLOBAL_EXECUTION_CLIENT_MAINNET in
   "geth.dnp.dappnode.eth")
     L1_RPC="http://geth.dappnode:8545"
+    L1_WS_RPC="ws://geth.dappnode:8546"
     ;;
   "nethermind.public.dappnode.eth")
     L1_RPC="http://nethermind.public.dappnode:8545"
+    L1_WS_RPC="ws://nethermind.public.dappnode:8546"
     ;;
   "erigon.dnp.dappnode.eth")
     L1_RPC="http://erigon.dappnode:8545"
+    L1_WS_RPC="ws://erigon.dappnode:8546"
     ;;
   "besu.public.dappnode.eth")
     L1_RPC="http://besu.public.dappnode:8545"
+    L1_WS_RPC="ws://besu.public.dappnode:8546"
     ;;
   *)
     echo "Unknown value for _DAPPNODE_GLOBAL_EXECUTION_CLIENT_MAINNET: $_DAPPNODE_GLOBAL_EXECUTION_CLIENT_MAINNET"
@@ -64,10 +68,12 @@ fi
 
 case $_DAPPNODE_GLOBAL_OP_EXECUTION_CLIENT in
 "op-geth.dnp.dappnode.eth")
+  L2_RPC="http://op-geth.dappnode:8545"
   L2_ENGINE="http://op-geth.dappnode:8551"
   JWT_PATH="/security/op-geth/jwtsecret.hex"
   ;;
 "op-erigon.dnp.dappnode.eth")
+  L2_RPC="http://op-geth.dappnode:8545"
   L2_ENGINE="http://op-erigon.dappnode:8551"
   JWT_PATH="/security/op-erigon/jwtsecret.hex"
   ;;
@@ -79,19 +85,24 @@ case $_DAPPNODE_GLOBAL_OP_EXECUTION_CLIENT in
 esac
 
 while true; do
-  op-node --network=op-mainnet \
-    --l1="$L1_RPC" \
-    --l1.beacon="$L1_BEACON_API" \
-    --l2="$L2_ENGINE" \
-    --l2.jwt-secret="$JWT_PATH" \
+  java --enable-preview \
+    --network=op-mainnet \
+    --l1-rpc-url="$L1_RPC" \
+    --l1-ws-rpc-url="$L1_WS_RPC" \
+    --l1-beacon-url="$L1_BEACON_API" \
+    --l2-rpc-url= "$L2_RPC" \
+    --l2-engine-url="$L2_ENGINE" \
+    --jwt-secret="$JWT_PATH" \
     --rpc.addr=0.0.0.0 \
     --rpc.port=9545 \
+    --log-level INFO \
+    --sync-mode full \
     ${EXTRA_FLAGS}
 
   STATUS=$?
 
   if [ $STATUS -ne 0 ]; then
-    echo "[ERROR - entrypoint ] op-node command failed with status $STATUS. Retrying in 1 minute..."
+    echo "[ERROR - entrypoint ] hildr command failed with status $STATUS. Retrying in 1 minute..."
     echo "[ERROR - entrypoint ] L2 Client might be unreachable or not ready yet (downloading chain data can take hours)"
     sleep 60
   else
